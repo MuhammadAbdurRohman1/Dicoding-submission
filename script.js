2c7801fedc1ff12764ab9d1e84cd9a8051b6c40b
Muhammadabdurrohman1 <tarnojepara6@gmail.com>
2023-05-31 14:13:40 +0700

    add buggy sidenav toggle

---
+++ script.js
@@ -0,0 +1,6 @@
+var hamb =document.getElementById("hamburger")
+var nav = document.getElementById("nav")
+
+function expand(){ nav.classList.toggle("open")  }
+document.body.addEventListener("click")
+console.log()
