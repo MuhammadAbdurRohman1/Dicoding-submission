914ede3a5e6baa7fb3506374f6d14fb2e982ebd5
Muhammadabdurrohman1 <tarnojepara6@gmail.com>
2023-06-03 23:42:21 +0700

    some styling

--- script.js
+++ script.js
@@ -9,12 +9,7 @@ document.body.addEventListener("click", function (e) {
   if (nav.classList.contains("open")){
     if(e.target != nav && e.target != hamb){
         expand()
-        console.log(e.target)
     }
   }
 } , true
 );
-
-console.log(nav.classList.contains("open"))
-
-console.log(hamb)
