7116831302da3e95bd5d5d3992c2e3b0ec70167d
Tukiman <70331295+MuhammadAbdurRohman1@users.noreply.github.com>
2023-06-04 03:00:56 +0700

    add blur

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var hamb = document.getElementById("hamburger") ;
 // hamb.addEventListener("click", expand);
 function expand() {
   nav.classList.toggle("open");
+  body.classList.toggle("blur")
 }
 document.body.addEventListener("click", function (e) {
   if (nav.classList.contains("open")){
