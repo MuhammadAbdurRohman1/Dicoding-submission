7aaf62f798f832dbf955b94a75d9c5cec51f6860
Muhammadabdurrohman1 <tarnojepara6@gmail.com>
2023-06-03 22:33:37 +0700

    fix buggy sidenav

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-var hamb =document.getElementById("hamburger")
-var nav = document.getElementById("nav")
+var nav = document.getElementById("nav");
+var hamb = document.getElementById("hamburger") ;
 
-function expand(){ nav.classList.toggle("open")  }
-document.body.addEventListener("click")
-console.log()
+// hamb.addEventListener("click", expand);
+function expand() {
+  nav.classList.toggle("open");
+}
+document.body.addEventListener("click", function (e) {
+  if (nav.classList.contains("open")){
+    if(e.target != nav && e.target != hamb){
+        expand()
+        console.log(e.target)
+    }
+  }
+} , true
+);
+
+console.log(nav.classList.contains("open"))
+
+console.log(hamb)
